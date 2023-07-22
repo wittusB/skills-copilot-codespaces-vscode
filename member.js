56df08ebb803b04cf53ff0762d436bd249c8e855
Nicolai Borre <61363673+wittusB@users.noreply.github.com>
2023-07-23 00:26:32 +0200

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+  return [
+    {
+      name: "HTML",
+      icon: <FaHtml5 />,
+    },
+    {
+      name: "CSS",
+      icon: <FaCss3Alt />,
+    },
+    {
+      name: "Javascript",
+      icon: <FaJs />,
+    },
+    {
+      name: "React",
+      icon: <FaReact />,
+    },
+    {
+      name: "Node",
+      icon: <FaNodeJs />,
+    },
+    {
+      name: "Python",
+      icon: <FaPython />,
+    },
+  ];
+} 
