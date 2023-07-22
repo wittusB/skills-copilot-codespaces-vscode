7e246b7c82b93bcb4a6ffc454555703c21a9f1ea
Nicolai Borre <61363673+wittusB@users.noreply.github.com>
2023-07-23 00:32:17 +0200

    Copilot third commit

--- comments.js
+++ comments.js
@@ -46,4 +46,4 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:3000/'); 
